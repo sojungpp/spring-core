6ef5fbc5aa4e2c7c5608dc6c9707e81749a143ea
sojungpp <sophie2458@naver.com>
2022-07-05 22:33:53 +0900

    chore: AppConfig 리팩토링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,11 +12,19 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository(); //memory로 쓸 것이라는 것 명확하게 보임! 추후 바꾸면 이 부분만 바꾸면 되는 것.
     }
 
     public OrderService orderService()  {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+    
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy(); //위 주석과 동일
     }
 
 
