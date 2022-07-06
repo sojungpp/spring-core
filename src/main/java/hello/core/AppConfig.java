d2621eb8055ef84c3979a8781f47aaa072a768f1
sojungpp <sophie2458@naver.com>
2022-07-06 21:36:01 +0900

    refactor: spring으로 바꾸기

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -2,29 +2,37 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository(); //memory로 쓸 것이라는 것 명확하게 보임! 추후 바꾸면 이 부분만 바꾸면 되는 것.
     }
 
+    @Bean
     public OrderService orderService()  {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
-    
+
+    @Bean
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy(); //위 주석과 동일
+        return new RateDiscountPolicy(); //위 주석과 동일
     }
 
 
